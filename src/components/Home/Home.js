86ce9d0de0441cac87aad77a6151484ed4c0d7d1
Alekasandr26 <a.brendin@mail.ru>
2021-09-18 16:42:22 +0400

    create search and cards

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Card } from "../Card/Card";
+import { Search } from "../Search/Search";
+
+const cards = new Array(15).fill("").map((_, i) => i);
 
 export const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
+  <Fragment>
+    <Search />
+    <div className="row mt-4">
+      {cards.map((card) => {
+        return (
+          <div className="col-sm-4 mb-4">
+            <Card key={card.i} />
+          </div>
+        );
+      })}
+      {/*<div className="col-sm-4 mb-4">
+        <Card />
+      </div>
+      <div className="col-sm-4 mb-4">
+        <Card />
+      </div>
+      <div className="col-sm-4 mb-4">
+        <Card />
+      </div>*/}
+    </div>
+  </Fragment>
 );
---
+++ src/components/Search/Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Search = () => (
+  <div className="form-group">
+    <input type="text" className="form-control" placeholder='Enter username' />
+  </div>
+);
