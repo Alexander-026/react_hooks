4d68540754dcdedfe246a61d2fd53f7239d9d919
Alekasandr26 <a.brendin@mail.ru>
2021-09-19 15:52:24 +0400

    add component alert 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { Alert } from "./components/Alert/Alert";
 import { Home } from "./components/Home/Home";
 import { Information } from "./components/Information/Information";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Profile } from "./components/Profile/Profile";
-import {AlertState} from './context/alert/alertState.js'
+import { AlertState } from "./context/alert/AlertState.js";
 
 function App() {
   return (
     <AlertState>
-      <Fragment>
-        <Navbar />
-        <div className="container pt-4">
-          <Alert alert={{ text: "test Alert" }} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/information" component={Information} />
-            <Route path="/profile" component={Profile} />
-            <Route />
-          </Switch>
-        </div>
-      </Fragment>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{ text: "test Alert" }} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/information" component={Information} />
+              <Route path="/profile" component={Profile} />
+              <Route />
+            </Switch>
+          </div>
+        </Fragment>
+      </BrowserRouter>
     </AlertState>
   );
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AlertContext } from "../../context/alert/alertContext";
 
-export const Search = () => (
-  <div className="form-group">
-    <input type="text" className="form-control" placeholder='Enter username' />
-  </div>
-);
+export const Search = () => {
+  const [value, setValue] =  useState('')
+  const { show } = useContext(AlertContext);
+  const onSubmit = (event) => {
+    if ((event.key !== "Enter")) {
+     return 
+    }
+
+    if(value.trim()) {
+      console.log('Make request with: ', value)
+    }else {
+      show('Enter user data !')
+    }
+  };
+
+  return (
+    <div className="form-group">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter username"
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
